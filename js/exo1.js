52b15d31dae4c2708bd6f7706d18f309d6909e16
Laura Munsch <laura.munsch@etu.unistra.fr>
2020-10-12 11:56:46 +0200

    cercles aléatoires au clic

--- js/exo1.js
+++ js/exo1.js
@@ -1,2 +1,19 @@
 const canvas = document.getElementById('exo1');
 const ctx = canvas.getContext('2d');
+
+function tracerCercle(x, y) {
+    let largeur = Math.random() * 100;
+    let color = 'rgb(10,' + Math.random() * 255 + ',' + Math.random() * 255 + ')';
+
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(x - 10, y - 80, largeur, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
+canvas.addEventListener('click', () => {
+    let mouseX = event.clientX;
+    let mouseY = event.clientY;
+
+    tracerCercle(mouseX, mouseY);
+})
