e1082a78cc8b49e6ce20e9a30dd924a40b1ef785
Laura Munsch <laura.munsch@etu.unistra.fr>
2020-10-12 19:06:33 +0200

    stocker et récupérer les cercles tracés

--- js/exo1.js
+++ js/exo1.js
@@ -1,19 +1,70 @@
+// initialisation du canvas
 const canvas = document.getElementById('exo1');
 const ctx = canvas.getContext('2d');
 
-function tracerCercle(x, y) {
-    let largeur = Math.random() * 100;
-    let color = 'rgb(10,' + Math.random() * 255 + ',' + Math.random() * 255 + ')';
+// on assigne le storage du navigateur à une constante
+const ls = window.localStorage;
 
-    ctx.fillStyle = color;
+// création du tableau contenant les paramètres des futurs cercles
+let cercles = [];
+
+function tracerCercle(x, y, rayon, couleur) {
+    ctx.fillStyle = couleur;
     ctx.beginPath();
-    ctx.arc(x - 10, y - 80, largeur, 0, 2 * Math.PI);
+    ctx.arc(x - 10, y - 80, rayon, 0, 2 * Math.PI);
     ctx.fill();
 }
 
-canvas.addEventListener('click', () => {
-    let mouseX = event.clientX;
-    let mouseY = event.clientY;
+function tracerAllCercles() {
+    cercles.forEach((val, i) => {
+        cercle = cercles[i];
+        tracerCercle(cercle.x, cercle.y, cercle.rayon, cercle.couleur);
+    });
+}
 
-    tracerCercle(mouseX, mouseY);
-})
+function creerCercle(mouseX, mouseY) {
+    // on défini les paramètres du cercle
+    let x = mouseX;
+    let y = mouseY;
+    let rayon = Math.floor(Math.random() * 100);
+    let couleur = 'rgb(10,' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ')';
+
+    // on trace le cercle
+    tracerCercle(x, y, rayon, couleur);
+
+    // on enregistre les paramètres du cercle dans un tableau
+    cercles[cercles.length] = {
+        x: x,
+        y: y,
+        rayon : rayon,
+        couleur: couleur
+    }
+}
+
+function saveCercles() {
+    cerclesToSave = JSON.stringify(cercles);
+    ls.setItem('cercles', cerclesToSave);
+}
+
+function getCercles() {
+    cerclesToReturn = JSON.parse(ls.getItem('cercles'));
+    return cerclesToReturn;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    cercles = getCercles();
+    tracerAllCercles();
+
+    canvas.addEventListener('click', (e) => {
+        // on récupère les coordonnées de la souris
+        let mouseX = e.clientX;
+        let mouseY = e.clientY;
+    
+        // on crée un cercle à cet endroit
+        creerCercle(mouseX, mouseY);
+    
+        // on enregistre les cercles dans le storage du navigateur
+        saveCercles();
+    });
+});
+
